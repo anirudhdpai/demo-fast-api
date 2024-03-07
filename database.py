d17f48842e41f6e1bd02208af577681782871552
anirudhdpai <anirudhpaid@gmail.com>
2024-03-07 22:04:58 +0530

    removed env

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:anirudh123@localhost/template1"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
